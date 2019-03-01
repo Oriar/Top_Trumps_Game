2fcb297f21dfa0d3269d3f046b03175331787740
Cli539 <47632988+Cli539@users.noreply.github.com>
2019-03-01 07:18:49 +0000

    Update Top_trumps_game.py

--- Top_trumps_game.py
+++ Top_trumps_game.py
@@ -1,3 +1,16 @@
+## Top_Trumps_Game
+## Python code for top trumps game on command line.
+
+## 01/03/2019 ;)
+
+# Created as an assignment by Chenyang Li and Jasamrit Rahala. 
+# This is code for a Top Trumps Game with two players.
+# The aim of the game is to win each round and win all the cards.
+# Each card has five attributes and these attributes are randomly generated. 
+# There are twenty cards, each with their own name. 
+# An attribute is chosen; if it is higher than the other person's attribute, the other player forfits a card to the first player. 
+# Winner is left with 20 cards
+
 import random as rnd
 import os
 import time as tm
